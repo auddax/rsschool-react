579f4aba230b018ad0e00e64b43585994f3454f4
Kirill Konovalov <auddax@gmail.com>
2023-03-19 23:34:21 +0300

    fix: delete .js files (Sun, Mar 19, 2023 11:34:21 PM)

--- src/components/Card/Card.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Card.module.css';
-class Card extends React.Component {
-    render() {
-        return React.createElement("article", { className: styles['card'] }, this.props.children);
-    }
-}
-export default Card;
