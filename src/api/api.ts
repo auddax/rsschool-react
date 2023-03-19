0dcea1759f3b706f96baa53a57f68d937d570661
Kirill Konovalov <auddax@gmail.com>
2023-03-19 22:21:59 +0300

    fix: add .env to .gitignore (Sun, Mar 19, 2023 10:21:59 PM)

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,4 @@
-// const apiKey = process.env.REACT_APP_API_KEY;
-const apiKey = 'AIzaSyAcD28mcYUpL2PMV4Rm1nx0HJnb5JJj4nU';
+const apiKey = import.meta.env.VITE_API_KEY;
 const url = `https://sheets.googleapis.com/v4/spreadsheets/19W3P5W8uXVw3XUseuqzn2l0phEZ_Yhx-nTbLNJfrj64/values/catalog?alt=json&key=${apiKey}`;
 
 const getProducts = async () => {
