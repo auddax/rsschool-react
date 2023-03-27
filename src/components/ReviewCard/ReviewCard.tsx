21cf7086115d5d4d6fea9d1f80397a6d60a937a7
Kirill Konovalov <auddax@gmail.com>
2023-03-27 11:46:45 +0300

    feat: add handling images in ReviewCard (Mon, Mar 27, 2023 11:46:45 AM)

--- src/components/ReviewCard/ReviewCard.tsx
+++ src/components/ReviewCard/ReviewCard.tsx
@@ -5,19 +5,25 @@ import styles from './ReviewCard.module.css';
 
 class ReviewCard extends React.Component<ReviewCardProps> {
   render(): ReactNode {
-    const { reviewText, purchaseDate, productType, rate, visibility, customerName } =
+    const { reviewText, purchaseDate, productType, rate, visibility, customerName, photo } =
       this.props.data;
+    const imgBlob = photo as Blob;
 
     return (
       <Card>
+        {imgBlob.size > 0 && (
+          <figure className={styles['card-img']}>
+            <img src={URL.createObjectURL(imgBlob)} alt="Customer photo" />
+          </figure>
+        )}
         <p className={styles['card-price']}>
           <>Отзыв: {reviewText}</>
         </p>
         <p className={styles['card-price']}>
-          <>Дата: {purchaseDate}</>
+          <>Дата покупки: {purchaseDate}</>
         </p>
         <p className={styles['card-price']}>
-          <>Категория: {productType}</>
+          <>Категория товара: {productType}</>
         </p>
         <p className={styles['card-price']}>
           <>Оценка: {rate}</>
