2cf375a817fade9105576bf71350c7596d1fdcfe
Kirill Konovalov <auddax@gmail.com>
2023-04-02 12:23:38 +0300

    refactor: convert Reviews component to functional component (Sun, Apr 02, 2023 12:23:38 PM)

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -1,33 +1,22 @@
-import React, { ReactNode } from 'react';
+import React, { useState } from 'react';
 import ReviewsList from '../ReviewsList';
 import ReviewsForm from '../ReviewsForm';
 import styles from './Reviews.module.scss';
 import { IReview } from 'types/interfaces';
 
-class Reviews extends React.Component<object, { reviews: IReview[] }> {
-  constructor(props: object) {
-    super(props);
-    this.state = {
-      reviews: [],
-    };
-    this.handleReviews = this.handleReviews.bind(this);
-  }
+const Reviews = () => {
+  const [reviews, setReviews] = useState<IReview[]>([]);
 
-  handleReviews(review: IReview): void {
-    this.setState((prevState) => ({
-      reviews: [...prevState.reviews, review],
-    }));
-  }
+  const handleReviews = (review: IReview) => {
+    setReviews((prevState: IReview[]) => [...prevState, review]);
+  };
 
-  render(): ReactNode {
-    const { reviews } = this.state;
-    return (
-      <section className={styles['reviews']}>
-        <ReviewsList reviews={reviews} />
-        <ReviewsForm handleReviews={this.handleReviews} />
-      </section>
-    );
-  }
-}
+  return (
+    <section className={styles['reviews']}>
+      <ReviewsList reviews={reviews} />
+      <ReviewsForm handleReviews={handleReviews} />
+    </section>
+  );
+};
 
 export default Reviews;
