f83cf68e79859edb660295cb3b870531e067027f
Kirill Konovalov <auddax@gmail.com>
2023-04-02 15:24:18 +0300

    refactor: convert ErrorMessage component to functional component (Sun, Apr 02, 2023 03:24:18 PM)

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,16 +1,14 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import styles from './ErrorMessage.module.scss';
 
-class ErrorMessage extends React.Component {
-  render(): ReactNode {
-    return (
-      <main className={styles['error-message']}>
-        <h1>Error!</h1>
-        <h2>Data cannot be loaded locally</h2>
-        <h2>Please follow the link https://rsschool-react.vercel.app/</h2>
-      </main>
-    );
-  }
-}
+const ErrorMessage = () => {
+  return (
+    <main className={styles['error-message']}>
+      <h1>Error!</h1>
+      <h2>Data cannot be loaded locally</h2>
+      <h2>Please follow the link https://rsschool-react.vercel.app/</h2>
+    </main>
+  );
+};
 
 export default ErrorMessage;
