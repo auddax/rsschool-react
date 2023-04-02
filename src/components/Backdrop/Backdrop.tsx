efe93aa683f497a7624c05a847d2764c2d706eb6
Kirill Konovalov <auddax@gmail.com>
2023-04-02 15:20:36 +0300

    refactor: convert Backdrop component to functional component (Sun, Apr 02, 2023 03:20:36 PM)

--- src/components/Backdrop/Backdrop.tsx
+++ src/components/Backdrop/Backdrop.tsx
@@ -1,16 +1,14 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { BackdropProps } from 'types/interfaces';
 import Modal from '../Modal';
 import styles from './Backdrop.module.scss';
 
-class Backdrop extends React.Component<BackdropProps> {
-  render(): ReactNode {
-    return (
-      <div className={styles['backdrop']}>
-        <Modal message={this.props.data} />
-      </div>
-    );
-  }
-}
+const Backdrop = (props: BackdropProps) => {
+  return (
+    <div className={styles['backdrop']}>
+      <Modal message={props.data} />
+    </div>
+  );
+};
 
 export default Backdrop;
