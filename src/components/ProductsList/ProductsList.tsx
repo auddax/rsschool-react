c59d2801e99427d280e3ef62f0a8e4219046a65f
Kirill Konovalov <auddax@gmail.com>
2023-04-02 15:57:54 +0300

    refactor: convert ProductsList component to functional component (Sun, Apr 02, 2023 03:57:54 PM)

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -1,19 +1,17 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import ProductCard from '../ProductCard';
 import { ProductsListProps } from 'types/interfaces';
 import styles from './ProductsList.module.scss';
 
-class ProductsList extends React.Component<ProductsListProps> {
-  render(): ReactNode {
-    const { products } = this.props;
-    return (
-      <div className={styles['products-list']}>
-        {products.map((product) => {
-          return <ProductCard data={product} key={product[1]} />;
-        })}
-      </div>
-    );
-  }
-}
+const ProductsList = (props: ProductsListProps) => {
+  const { products } = props;
+  return (
+    <div className={styles['products-list']}>
+      {products.map((product) => {
+        return <ProductCard data={product} key={product[1]} />;
+      })}
+    </div>
+  );
+};
 
 export default ProductsList;
