e61b5660ea6a92133ce1a34505c90d4778558991
Kirill Konovalov <auddax@gmail.com>
2023-04-02 18:00:55 +0300

    refactor: convert SuccessModal component to functional component (Sun, Apr 02, 2023 06:00:55 PM)

--- src/components/SuccessModal/SuccessModal.tsx
+++ src/components/SuccessModal/SuccessModal.tsx
@@ -1,13 +1,11 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { SuccessModalProps } from 'types/interfaces';
 import Backdrop from '../Backdrop';
 
-class SuccessModal extends React.Component<SuccessModalProps> {
-  render(): ReactNode {
-    const container = document.getElementById('backdrop-root') as Element;
-    return <>{ReactDOM.createPortal(<Backdrop data={this.props.data} />, container)}</>;
-  }
-}
+const SuccessModal = (props: SuccessModalProps) => {
+  const container = document.getElementById('backdrop-root') as Element;
+  return <>{ReactDOM.createPortal(<Backdrop data={props.data} />, container)}</>;
+};
 
 export default SuccessModal;
