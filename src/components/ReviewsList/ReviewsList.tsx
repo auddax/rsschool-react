d55d702a52ae9b2acfcc3ec3fee0268d22a79f11
Kirill Konovalov <auddax@gmail.com>
2023-04-02 16:00:35 +0300

    refactor: convert ReviewsList component to functional component (Sun, Apr 02, 2023 04:00:35 PM)

--- src/components/ReviewsList/ReviewsList.tsx
+++ src/components/ReviewsList/ReviewsList.tsx
@@ -1,19 +1,17 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { ReviewsListProps } from 'types/interfaces';
 import ReviewCard from '../ReviewCard';
 import styles from './ReviewsList.module.scss';
 
-class ReviewsList extends React.Component<ReviewsListProps> {
-  render(): ReactNode {
-    const { reviews } = this.props;
-    return (
-      <div className={styles['reviews-list']}>
-        {reviews.map((review, index) => {
-          return <ReviewCard data={review} key={'review' + index} />;
-        })}
-      </div>
-    );
-  }
-}
+const ReviewsList = (props: ReviewsListProps) => {
+  const { reviews } = props;
+  return (
+    <div className={styles['reviews-list']}>
+      {reviews.map((review, index) => {
+        return <ReviewCard data={review} key={'review' + index} />;
+      })}
+    </div>
+  );
+};
 
 export default ReviewsList;
