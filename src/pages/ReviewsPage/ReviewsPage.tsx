18d6e309938734740ec2b9b4f608defeaf3ca689
Kirill Konovalov <auddax@gmail.com>
2023-04-02 15:15:26 +0300

    refactor: convert ReviewsPage component to functional component (Sun, Apr 02, 2023 03:15:26 PM)

--- src/pages/ReviewsPage/ReviewsPage.tsx
+++ src/pages/ReviewsPage/ReviewsPage.tsx
@@ -1,16 +1,14 @@
+import React from 'react';
 import Reviews from '../../components/Reviews';
-import React, { ReactNode } from 'react';
 import Header from '../../components/Header';
 
-class ReviewsPage extends React.Component {
-  render(): ReactNode {
-    return (
-      <>
-        <Header />
-        <Reviews />
-      </>
-    );
-  }
-}
+const ReviewsPage = () => {
+  return (
+    <>
+      <Header />
+      <Reviews />
+    </>
+  );
+};
 
 export default ReviewsPage;
